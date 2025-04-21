db6a741debd0525e32f3ef2f28b89ab11fee82a4
Tall <diarnotall@gmail.com>
2025-04-21 15:24:33 +0000

    Auto-generated commit

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,15 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>Afri-News</title>
+  <base href="/RssWebApp/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://unpkg.com/primeflex@latest/primeflex.css"> 
+<style>@-webkit-keyframes fa-spin{0%{-webkit-transform:rotate(0deg);transform:rotate(0)}to{-webkit-transform:rotate(359deg);transform:rotate(359deg)}}body{margin:0!important;padding:0!important}</style><link rel="stylesheet" href="styles-TPIZWHG4.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-TPIZWHG4.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<link rel="modulepreload" href="chunk-E355VWVU.js"><script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-DN4A35KL.js" type="module"></script></body>
+</html>
+`;
